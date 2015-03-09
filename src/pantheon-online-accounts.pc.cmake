2dae356bb370cba18d00866c40dcd25ef584c622
Corentin Noël <corentin@elementary.io>
2015-03-09 23:55:15 +0100

    Now usable as a library.

--- src/pantheon-online-accounts.pc.cmake
+++ src/pantheon-online-accounts.pc.cmake
@@ -6,7 +6,7 @@ includedir=@DOLLAR@{prefix}/include/
 Name: Online Accounts (for the Pantheon Desktop)
 Description: Online Accounts headers  
 Version: 0.1  
-Libs: -lonline-accounts-pantheon
-Cflags: -I@DOLLAR@{includedir}/online-accounts-pantheon
+Libs: -l@PLUGNAME@
+Cflags: -I@DOLLAR@{includedir}/@PLUGNAME@
 Requires: glib-2.0 gio-2.0 gee-0.8 libpeas-1.0 gtk+-3.0 granite libaccounts-glib libgsignon-glib
 
